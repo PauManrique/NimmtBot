8897a8f8e9a7d490c5e309037737b75c5e0aac9f
Pau Manrique <pau.manrique@thoughtworks.com>
2022-09-12 17:29:33 +0200

    return closest card for safe lines

--- nimmt_bot/nimmt_bot.py
+++ nimmt_bot/nimmt_bot.py
@@ -1,3 +1,17 @@
 class NimmtBot:
-    def calculate_move(self):
-        return "RANDOM"
+  MAX_CARDS_PER_LINE = 5
+
+  def calculate_move(self, line_1, line_2, line_3, line_4, hand_cards):
+    safe_lines = []
+    for line in line_1, line_2, line_3, line_4:
+      if len(line) < self.MAX_CARDS_PER_LINE:
+        safe_lines.append(line)
+    min_card_value_diff = 200    
+    for line in safe_lines:
+      line_last_card = line[len(line) - 1]
+      for card in hand_cards:
+        diff = card - line_last_card
+        if card > line_last_card and diff < min_card_value_diff:
+          min_card_value_diff = diff
+          card_to_play = card
+    return "PLAY " + str(card_to_play)
--- test/test_nimmt_bot.py
+++ test/test_nimmt_bot.py
@@ -1,6 +1,11 @@
 from nimmt_bot.nimmt_bot import NimmtBot
 
 
-def test_random():
+def test_should_play_the_next_consecutive_card():
+    line_1 = [1, 2, 3, 4, 5]
+    line_2 = [6, 7, 8, 9, 10]
+    line_3 = [11, 12, 13, 14]
+    line_4 = [16, 17, 18, 19, 20]
+    hand_card = [15, 24, 36, 47, 50]
     bot = NimmtBot()
-    assert "RANDOM" == bot.calculate_move()
+    assert "PLAY 15" == bot.calculate_move(line_1, line_2, line_3, line_4, hand_card)
