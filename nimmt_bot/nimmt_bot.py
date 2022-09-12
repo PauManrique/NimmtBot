6b636769a53b69c89bdc13ce474d90e5257802d5
Pau Manrique <pau.manrique@thoughtworks.com>
2022-09-12 17:34:45 +0200

    add fallback to random

--- nimmt_bot/nimmt_bot.py
+++ nimmt_bot/nimmt_bot.py
@@ -6,6 +6,7 @@ class NimmtBot:
     for line in line_1, line_2, line_3, line_4:
       if len(line) < self.MAX_CARDS_PER_LINE:
         safe_lines.append(line)
+    card_to_play = None
     min_card_value_diff = 200    
     for line in safe_lines:
       line_last_card = line[len(line) - 1]
@@ -14,4 +15,6 @@ class NimmtBot:
         if card > line_last_card and diff < min_card_value_diff:
           min_card_value_diff = diff
           card_to_play = card
+    if card_to_play is None:
+      return "RANDOM"      
     return "PLAY " + str(card_to_play)
--- test/test_nimmt_bot.py
+++ test/test_nimmt_bot.py
@@ -9,3 +9,12 @@ def test_should_play_the_next_consecutive_card():
     hand_card = [15, 24, 36, 47, 50]
     bot = NimmtBot()
     assert "PLAY 15" == bot.calculate_move(line_1, line_2, line_3, line_4, hand_card)
+
+def test_should_fallback_to_random():
+    line_1 = [1, 2, 3, 4, 5]
+    line_2 = [6, 7, 8, 9, 10]
+    line_3 = [11, 12, 13, 14, 15]
+    line_4 = [16, 17, 18, 19, 20]
+    hand_card = [30, 24, 36, 47, 50]
+    bot = NimmtBot()
+    assert "RANDOM" == bot.calculate_move(line_1, line_2, line_3, line_4, hand_card)    
